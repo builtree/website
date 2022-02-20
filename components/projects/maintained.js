e3ffc70d30d4a2ab03efb2e6b215dd0a9105ec08
Yash Lamba <yashlamba2000@gmail.com>
2022-02-20 17:39:39 +0530

    Added margin

--- components/projects/maintained.js
+++ components/projects/maintained.js
@@ -6,7 +6,7 @@ function Maintained() {
             <h2 className="text-xl font-bold text-[#0B4870] underline underline-offset-8 decoration-[#FFA62B] text-center pt-12">
                 Maintained Projects
             </h2>
-            <div className="grid justify-items-center my-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="grid justify-items-center mx-4 my-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 <ProjectCard title={'Simulate'} description={'A collection of simulations and visualizations for all sorts of stuff (Majorly Algorithmic or Mathematical)'} github={'https://github.com/builtree/simulate'} deployment={'https://www.builtree.org/simulate/web/'} image={'https://raw.githubusercontent.com/builtree/assets/simulate/documentation/SimulateTitle.svg'}/>
                 <ProjectCard title={'Handwrite'} description={'Handwrite generates a custom font based on your handwriting sample.'} github={'https://github.com/builtree/handwrite'} deployment={'https://builtree.org/handwrite-web'} image={'https://raw.githubusercontent.com/builtree/handwrite-web/dev/public/logo512.png'}/>
             </div>
